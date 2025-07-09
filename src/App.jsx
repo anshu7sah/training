66d25106c94f8e508be3a352bd5d57ba2d420ea2
Anshu Sah <anshu7sah@gmail.com>
2025-07-09 10:26:42 +0545

    debouncing using useEffect and lodash

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,51 @@
-import { useCallback, useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
-import debounce from "lodash.debounce"; // or your custom debounce utility
 
 function App() {
-  const debouncedSearch = debounce((query) => {
-    // Perform your search or API call here
-    console.log("Debounced search for:", query);
-  }, 500);
+  const [data, setData] = useState(1);
+  const [u, setU] = useState(2);
+  const [name, setName] = useState("");
 
-  const handleChange = useCallback(
-    (event) => {
-      debouncedSearch(event.target.value);
-    },
-    [debouncedSearch]
-  ); // Include debouncedSearch in dependency array if it could change
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      console.log("name", name);
+    }, 500);
 
-  return <input type="text" onChange={handleChange} />;
+    return () => clearTimeout(timer);
+  }, [name]);
+
+  const clickHandler = () => {
+    setData((p) => p + 1);
+  };
+
+  const uclickHandler = () => {
+    setU((p) => p + 1);
+  };
+
+  return (
+    <div>
+      {data}
+      <br /> {u}
+      <br />
+      <button onClick={clickHandler}>click</button>
+      <br />
+      <button onClick={uclickHandler}>Uclick</button>
+      <input type="text" onChange={(e) => setName(e.target.value)} />
+    </div>
+  );
 }
 
+// function App() {
+//   const debouncedSearch = debounce((query) => {
+//     console.log("Debounced search for:", query);
+//   }, 500);
+
+//   const handleChange = (event) => {
+//     debouncedSearch(event.target.value);
+//   };
+
+//   // Include debouncedSearch in dependency array if it could change
+
+//   return <input type="text" onChange={handleChange} />;
+// }
 export default App;
